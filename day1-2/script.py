5c1bbad55f2a737d41e95cb24e25fcd91848aae9
Oscar Ekeroth <oekeroth@baffinbaynetworks.com>
2020-12-01 14:02:47 +0100

    Smaller day1-2 script

--- day1-2/script.py
+++ day1-2/script.py
@@ -1,11 +1,7 @@
 with open('input') as f:
     input_lines = [int(line.rstrip()) for line in f]
-done = False
 for input in input_lines:
-    if done:
-        break
     for input2 in input_lines:
         if 2020 - input - input2 in input_lines:
             print(input * input2 * (2020 - input - input2))
-            done = True
-            break
+            exit()
